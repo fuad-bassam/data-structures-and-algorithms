a9f23f737daae6d6d40edca17aa995f316e3b365
fuad-bassam <74737369+fuad-bassam@users.noreply.github.com>
2022-03-14 16:42:41 +0300

    solve with Whiteboard

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using ConsoleApp1.codeCha;
+
 
 namespace ConsoleApp1
 {
+
+
+    
+
+
     class Program
     {
+
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+
+            ///////array-reverse challenge           
+            int[] arr = { 5, 6, 7 };
+            Class1.showArray(Class1.reverseArray(arr));
+             
+
+            //////////////
         }
     }
 }
