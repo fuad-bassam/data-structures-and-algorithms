1112e8378910f83b4fe8e2946ab4e7f3caf8944f
fuad-bassam <74737369+fuad-bassam@users.noreply.github.com>
2022-04-06 18:02:12 +0300

    Solve Stack & Queue classes

---
+++ ConsoleApp1/CodeChallenge/stack-and-queue/Queue.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+   public class Queue
+    {
+
+        Node  front = new Node();
+
+        Node rear;
+
+
+        /// <summary>
+        /// to add elemnt at the first of Queue
+        /// </summary>
+        /// <param name="value"></param>
+        public void enqueue(String value)
+     {
+            Node node = new Node(value);
+            if (front.Value == null)
+            {
+                front = node;
+                rear = front;
+            }
+            else
+            {
+                rear.next = node;
+                rear = rear.next;
+            }
+            Console.WriteLine($"Enqueue {value} done successfully");
+
+        }
+
+        /// <summary>
+        /// to renove the last element in the Queue
+        /// </summary>
+        /// <returns></returns>
+     public string dequeue()
+     {
+            Node node = front;
+            if (front == null)
+            {
+                //  return null;
+                return "Queue is empty !!";
+            }
+            front = front.next;
+            if (isEmpty())
+            {
+                rear = null;
+            }
+            return node.Value;
+
+
+      }
+     public String peek()
+     {
+            if (isEmpty())
+            {
+                return "Queue is empty !!";
+            }
+
+            return rear.Value;
+
+
+     }
+        /// <summary>
+        ///  to check if the Queue is null or not
+        /// </summary>
+        /// <returns></returns>
+        public bool isEmpty()
+     {
+            if (front == null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+
+      }
+
+
+    }
+}
---
+++ ConsoleApp1/CodeChallenge/stack-and-queue/Stack.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+
+
+    //public class Node2<T>
+    //{
+
+    //    //int value;
+    //    public Node2<T> next;
+
+    //    public T Value { get; set; }
+
+    //    // public Node Next { get; set; }
+
+    //    public Node2(T val)
+    //    {
+    //        this.Value = val;
+
+    //        this.next = null;
+    //    }
+    //    public Node2()
+    //    {
+
+    //        this.Value = null;
+
+    //        this.next = null;
+    //    }
+    //}
+
+    public class Stack
+    {
+
+        //FILO
+        Node top = new Node();
+
+        /// <summary>
+        /// method push to add elemnt in the stack 
+        /// </summary>
+        /// <param name="value">value to add it in the Stack</param>
+        public void push(String value)
+        {
+            Node node = new Node(value);
+
+             node.next= top;
+            top = node;
+            Console.WriteLine($"Push {value} done successfully");
+        }
+
+        /// <summary>
+        /// pop to return and remove from the stack
+        /// </summary>
+        /// <returns></returns>
+        public string pop()
+        {
+            if (top.Value == null)
+            {
+                // return null;
+                return "Stack is empty !!";
+            }
+
+            String x = top.Value;
+            top = top.next;
+            return x;
+
+        }
+
+
+        /// <summary>
+        /// to return the value of the top of the stack (first elemnet)
+        /// </summary>
+        /// <returns></returns>
+        public string peek()
+        {
+
+            if (top.Value == null)
+            {
+                return "Stack is empty !!";
+            }
+
+
+            return top.Value;
+
+
+        }
+
+        /// <summary>
+        /// to check if the Stack is null or not
+        /// </summary>
+        /// <returns></returns>
+        public bool isEmpty()
+        {
+            if (top.Value == null )
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+
+        }
+
+    }
+}
